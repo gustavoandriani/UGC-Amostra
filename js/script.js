84153ebc07e18ca5920d2c01bb66fcefc3e0dff1
Gustavo Andriani <90989236+gustavoandriani@users.noreply.github.com>
2023-06-05 15:50:47 -0300

    sessão cadastros concluida telas maiores

--- js/script.js
+++ js/script.js
@@ -87,15 +87,70 @@ function cadastroCosplayInfoAnterior() {
   }
 }
 
-function switchCosplayInfos() {
-  const cosplayInfos = document.getElementById('cosplay-infos')
 
-  if (cosplayInfos.classList == 'container', 'd-none') {
-    cosplayInfos.classList.remove('d-none')
-    cosplayInfos.classList.add('d-block')
-  }
-  else {
-    cosplayInfos.classList.remove('d-block')
-    cosplayInfos.classList.add('d-none') 
-  }
+//MEDIA QUERIES E ALTERAÇÃO DE FUNÇÕES
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+//window.addEventListener("DOMContentLoaded", (event) => {
+//    const btnCospMob = document.getElementById('cosplay-btn-mob')
+//    btnCosp.addEventListener('click', function(event){
+//      event.preventDefault();
+//    })
+//  })
+
+function abrirPopUp(divId) {
+  var url = "about:blank";
+    var nomeJanela = "TESTE POPUP";
+    var largura = 500;
+    var altura = 300;
+    var posicaoEsquerda = (window.screen.width - largura) / 2;
+    var posicaoTopo = (window.screen.height - altura) / 2;
+
+    var janelaPopUp = window.open(url, nomeJanela, "width=" + largura + ", height=" + altura + ", left=" + posicaoEsquerda + ", top=" + posicaoTopo);
+
+    if (janelaPopUp === null || typeof(janelaPopUp) === "undefined") {
+        alert("O pop-up foi bloqueado pelo navegador. Por favor, habilite pop-ups para este site.");
+    } else {
+        // Personalize o conteúdo da janela pop-up aqui
+        janelaPopUp.document.write("<h1>Este é o conteúdo do pop-up</h1>");
+    }
 }
+
+function switchInfos(divId) {
+  var div = document.getElementById(divId);
+  if (window.matchMedia("(max-width:800px)").matches) {
+    abrirPopUp()
+  } else {
+    if (div.style.display === 'none') {
+      div.style.display = 'block';
+      const divCosplay = document.getElementById('cosplay-infos')
+      const divKpop = document.getElementById('kpop-infos')
+      const divBeco = document.getElementById('beco-infos')
+      if (div === divCosplay) {
+        if (divKpop.style.display === 'block') {
+          divKpop.style.display = 'none'
+        } else if (divBeco.style.display === 'block') {
+          divBeco.style.display = 'none'
+        }
+        
+      }
+      else if (div === divKpop) {
+        if (divBeco.style.display === 'block') {
+          divBeco.style.display = 'none'
+        } else if (divCosplay.style.display === 'block') {
+          divCosplay.style.display = 'none'
+        }
+        
+      }
+      else if (div === divBeco) {
+        if (divCosplay.style.display === 'block') {
+          divCosplay.style.display = 'none'
+        } else if (divKpop.style.display === 'block') {
+          divKpop.style.display = 'none'
+        }
+      }
+    } else {
+      div.style.display = 'none';
+    }
+  }
+};
