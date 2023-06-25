b6fd9952a78275f8234182f4ac1fc4f90b256d0d
Gustavo Andriani <90989236+gustavoandriani@users.noreply.github.com>
2023-06-24 21:18:08 -0300

    Rodapé criado e ajustes feitos

--- js/script.js
+++ js/script.js
@@ -2,38 +2,38 @@ function convidadoSelecao(event, id) {
   const convidadoPainel = document.getElementById('painel-convidados-pocket')
   if (id == "box-AH") {
     convidadoPainel.classList.add('AH')
-    if (convidadoPainel.classList == 'JP', 'CH', 'LB', 'PDT', 'AC') {
-      convidadoPainel.classList.remove('JP', 'CH', 'LB', 'PDT', 'AC')
+    if (convidadoPainel.classList == 'JP', 'KB', 'LB', 'PDT', 'AC') {
+      convidadoPainel.classList.remove('JP', 'KB', 'LB', 'PDT', 'AC')
     }
   }
   else if (id == "box-JP") {
     convidadoPainel.classList.add('JP')
-    if (convidadoPainel.classList == 'AH', 'CH', 'LB', 'PDT', 'AC'){
-      convidadoPainel.classList.remove('AH', 'CH', 'LB', 'PDT', 'AC')
+    if (convidadoPainel.classList == 'AH', 'KB', 'LB', 'PDT', 'AC'){
+      convidadoPainel.classList.remove('AH', 'KB', 'LB', 'PDT', 'AC')
     }
   }
-  else if (id == "box-CH"){
-    convidadoPainel.classList.add('CH')
+  else if (id == "box-KB"){
+    convidadoPainel.classList.add('KB')
     if (convidadoPainel.classList == 'AH', 'JP', 'LB', 'PDT', 'AC'){
       convidadoPainel.classList.remove('AH', 'JP', 'LB', 'PDT', 'AC')
     }
   }
   else if (id == "box-LB"){
     convidadoPainel.classList.add('LB')
-    if (convidadoPainel.classList == 'AH', 'JP', 'CH', 'PDT', 'AC'){
-      convidadoPainel.classList.remove('AH', 'JP', 'CH', 'PDT', 'AC')
+    if (convidadoPainel.classList == 'AH', 'JP', 'KB', 'PDT', 'AC'){
+      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'PDT', 'AC')
     }
   }
   else if (id == "box-PDT"){
     convidadoPainel.classList.add('PDT')
-    if (convidadoPainel.classList != 'AH', 'JP', 'CH', 'LB', 'AC'){
-      convidadoPainel.classList.remove('AH', 'JP', 'CH', 'LB', 'AC')
+    if (convidadoPainel.classList != 'AH', 'JP', 'KB', 'LB', 'AC'){
+      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'LB', 'AC')
     }
   }
   else if (id == "box-AC"){
     convidadoPainel.classList.add('AC')
-    if (convidadoPainel.classList != 'AH', 'JP', 'CH', 'LB', 'PDT'){
-      convidadoPainel.classList.remove('AH', 'JP', 'CH', 'LB', 'PDT')
+    if (convidadoPainel.classList != 'AH', 'JP', 'KB', 'LB', 'PDT'){
+      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'LB', 'PDT')
     }
   }
 }
