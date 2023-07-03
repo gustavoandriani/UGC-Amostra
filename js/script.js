f6474a847f48ed0e6fc02eb41fe7340f886c9f6d
Gustavo Andriani <90989236+gustavoandriani@users.noreply.github.com>
2023-07-03 13:32:28 -0300

    finalizado

--- js/script.js
+++ js/script.js
@@ -2,38 +2,44 @@ function convidadoSelecao(event, id) {
   const convidadoPainel = document.getElementById('painel-convidados-pocket')
   if (id == "box-AH") {
     convidadoPainel.classList.add('AH')
-    if (convidadoPainel.classList == 'JP', 'KB', 'LB', 'PDT', 'AC') {
-      convidadoPainel.classList.remove('JP', 'KB', 'LB', 'PDT', 'AC')
+    if (convidadoPainel.classList == 'JP', 'KB', 'LB', 'PDT', 'AC', 'IB') {
+      convidadoPainel.classList.remove('JP', 'KB', 'LB', 'PDT', 'AC', 'IB')
     }
   }
   else if (id == "box-JP") {
     convidadoPainel.classList.add('JP')
-    if (convidadoPainel.classList == 'AH', 'KB', 'LB', 'PDT', 'AC'){
-      convidadoPainel.classList.remove('AH', 'KB', 'LB', 'PDT', 'AC')
+    if (convidadoPainel.classList == 'AH', 'KB', 'LB', 'PDT', 'AC', 'IB'){
+      convidadoPainel.classList.remove('AH', 'KB', 'LB', 'PDT', 'AC', 'IB')
     }
   }
-  else if (id == "box-KB"){
-    convidadoPainel.classList.add('KB')
-    if (convidadoPainel.classList == 'AH', 'JP', 'LB', 'PDT', 'AC'){
-      convidadoPainel.classList.remove('AH', 'JP', 'LB', 'PDT', 'AC')
+  else if (id == "box-IB"){
+    convidadoPainel.classList.add('IB')
+    if (convidadoPainel.classList == 'AH', 'JP', 'LB', 'PDT', 'AC', 'KB'){
+      convidadoPainel.classList.remove('AH', 'JP', 'LB', 'PDT', 'AC', 'KB')
     }
   }
   else if (id == "box-LB"){
     convidadoPainel.classList.add('LB')
-    if (convidadoPainel.classList == 'AH', 'JP', 'KB', 'PDT', 'AC'){
-      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'PDT', 'AC')
+    if (convidadoPainel.classList == 'AH', 'JP', 'KB', 'PDT', 'AC', 'IB'){
+      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'PDT', 'AC', 'IB')
     }
   }
   else if (id == "box-PDT"){
     convidadoPainel.classList.add('PDT')
-    if (convidadoPainel.classList != 'AH', 'JP', 'KB', 'LB', 'AC'){
-      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'LB', 'AC')
+    if (convidadoPainel.classList != 'AH', 'JP', 'KB', 'LB', 'AC', 'IB'){
+      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'LB', 'AC', 'IB')
     }
   }
   else if (id == "box-AC"){
     convidadoPainel.classList.add('AC')
-    if (convidadoPainel.classList != 'AH', 'JP', 'KB', 'LB', 'PDT'){
-      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'LB', 'PDT')
+    if (convidadoPainel.classList != 'AH', 'JP', 'KB', 'LB', 'PDT', 'IB'){
+      convidadoPainel.classList.remove('AH', 'JP', 'KB', 'LB', 'PDT', 'IB')
+    }
+  }
+  else if (id == "box-KB"){
+    convidadoPainel.classList.add('KB')
+    if (convidadoPainel.classList == 'AH', 'JP', 'LB', 'PDT', 'AC', 'IB'){
+      convidadoPainel.classList.remove('AH', 'JP', 'LB', 'PDT', 'AC', 'IB')
     }
   }
 }
